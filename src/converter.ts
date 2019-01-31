1accfc2a60c904692d4ff38f8ee69efa2532f29b
Davide Gheri <davide@davidegheri.com>
2019-01-31 17:12:33 +0100

    fix casting error, SVG props width and height should be string

--- src/converter.ts
+++ src/converter.ts
@@ -1,8 +1,17 @@
 import React from 'react';
 import { AbstractElement, SizeProp } from '@fortawesome/fontawesome-svg-core';
-import { Svg, Path, Defs, Rect, Mask, ClipPath, G, Circle } from 'react-native-svg';
+import {
+  Svg,
+  Path,
+  Defs,
+  Rect,
+  Mask,
+  ClipPath,
+  G,
+  Circle
+} from 'react-native-svg';
 
-export const mapping: {[key: string]: React.ComponentClass<any, any>} = {
+export const mapping: { [key: string]: React.ComponentClass<any, any> } = {
   svg: Svg,
   path: Path,
   rect: Rect,
@@ -10,10 +19,10 @@ export const mapping: {[key: string]: React.ComponentClass<any, any>} = {
   defs: Defs,
   mask: Mask,
   clipPath: ClipPath,
-  circle: Circle,
+  circle: Circle
 };
 
-export function sizeToInt(size?: SizeProp) {
+export function sizeToInt(size?: SizeProp): number {
   const base = 24;
   switch (true) {
     case size === 'xs':
@@ -22,7 +31,8 @@ export function sizeToInt(size?: SizeProp) {
       return (base * 4) / 3;
     case size === 'sm':
       return base * 0.875;
-    case typeof size === 'string' && (size as string).indexOf('x') === (size as string).length - 1:
+    case typeof size === 'string' &&
+      (size as string).indexOf('x') === (size as string).length - 1:
       const multi = parseInt((size as string).replace('x', ''), 10);
       return base * multi;
     default:
@@ -30,20 +40,28 @@ export function sizeToInt(size?: SizeProp) {
   }
 }
 
-export function convert(tree: AbstractElement, otherProps: {size?: SizeProp; color?: string} = {}, key?: string | number) {
-  const childrens: any = tree.children ? tree.children.map((t, k) => convert(t, {color: otherProps.color}, k + 1)) : [];
+export function convert(
+  tree: AbstractElement,
+  otherProps: { size?: SizeProp; color?: string } = {},
+  key?: string | number
+) {
+  const childrens: any = tree.children
+    ? tree.children.map((t, k) =>
+        convert(t, { color: otherProps.color }, k + 1)
+      )
+    : [];
 
   const { size, color } = otherProps;
 
   const props = Object.assign(
     {},
     tree.attributes,
-    key ? {key} : null,
-    color ? {fill: color} : null,
+    key ? { key } : null,
+    color ? { fill: color } : null,
     {
-      width: sizeToInt(size),
-      height: sizeToInt(size),
-    },
+      width: sizeToInt(size).toString(),
+      height: sizeToInt(size).toString()
+    }
   );
 
   return React.createElement(mapping[tree.tag], props, childrens);
